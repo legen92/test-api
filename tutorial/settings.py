5c98fd92352b2794ec4c78725a58e0ac57da5b21
unknown <confessional111@gmail.comm>
2023-02-21 23:49:52 +0700

    test lan cuoi

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%#^h@86-xpu@-ds+=#1q!$w0ql$p%50f2p6wt#ye@@%%z2$5fw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app']
 
